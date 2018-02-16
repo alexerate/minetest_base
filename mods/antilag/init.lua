acb78378a1fecc3a2967eb6946c4962355a769c8
alexerate <35458285+alexerate@users.noreply.github.com>
2018-02-16 18:36:54 +0100

    remove crash when player=nil

--- mods/antilag/init.lua
+++ mods/antilag/init.lua
@@ -6,6 +6,7 @@ end
 
 minetest.register_on_protection_violation(function(pos,name)
   local player = minetest.get_player_by_name(name)
+  if not player then return end
   local playerpos = player:getpos()
   if areas and not areas:canInteract(pos,name) or not default.can_interact_with_node(player,pos) then
     minetest.after(1,antilag.rollback,{player=player,playerpos=playerpos})
